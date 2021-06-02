d5e7f4e3bf98001cf2f7a4ec897e242354d279c6
Taeheon-Lee <taeheon714@gmail.com>
2021-06-02 13:17:31 +0900

    3053

---
+++ 3053.py
@@ -0,0 +1,10 @@
+"Question 3053"
+
+# Explaining easily, Taxicab geometry is a distance consisted vertically connected lines.
+
+import math
+
+R = int(input())
+
+print(R ** 2 * math.pi)
+print((2 * R) ** 2 / 2)
