cbf5422a929c684ebc20c12c8286682c7b45a3e9
TaeHeon-Lee <taeheon714@gmail.com>
2021-06-30 00:45:19 +0900

    time error 9997

--- 9997.py
+++ 9997.py
@@ -4,21 +4,25 @@ def check(string):
     tmp = [0] * 26
     for elem in string:
         tmp[ord(elem)-ord('a')] += 1
-    if 0 not in tmp:
-        return True
+        if 0 not in tmp:
+            return True
     return False
 
-def make_sentance(string, cnt, k):
+def make_sentance(string, cnt, k, sign):
     global answer
     if cnt > 0:
-        if check(string) is True:
+        if sign == 1:
             answer += 1
-    back = string[:]
+        elif check(string) is True:
+            answer += 1
+            sign = 1
     for i in range(k, len(lst)):
+        back = string[:]
         string += lst[i]
-        make_sentance(string, cnt + 1, k + 1)
-        make_sentance(back, 0, i)
-        
+        make_sentance(string, cnt + 1, i + 1, sign)
+        string = back
+        cnt = 0
+        sign = 0
 
 N = int(input())
 lst = []
@@ -26,4 +30,5 @@ answer = 0
 for _ in range(N):
     lst.append(input())
 lst = list(set(lst))
-make_sentance("", 0, 0)
+make_sentance("", 0, 0, 0)
+print(answer)
